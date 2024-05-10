a136043aea102c2809647576f6e34c70baadaa89
Chris Lattman <natgeo13@gmail.com>
2024-05-09 20:55:10 -0400

    update

--- testdriver.c
+++ testdriver.c
@@ -3,6 +3,9 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/ioctl.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/spinlock.h>
 
 #include "testdriver.h"
 
@@ -15,6 +18,8 @@ static int char_dev_major_number = 0;
 static const char *const name = "testdevice";
 static int answer = 0;
 static struct mystruct test = {0, ""};
+static char *string;
+static DEFINE_SPINLOCK(myspinlock);
 
 static int driver_open(struct inode *dev_file, struct file *instance)
 {
@@ -29,17 +34,20 @@ static int driver_release(struct inode *dev_file, struct file *instance)
 // the return value of this function is the return value of ioctl() in user.c
 static long driver_unlocked_ioctl(struct file *file, unsigned int request, unsigned long arg)
 {
+    int retval = 0;
+
+    spin_lock(&myspinlock); // copy_to/from_user should be fast
     switch (request) {
     case RD_VALUE:
         if (copy_to_user((int *) arg, &answer, sizeof(answer)) != 0) {
             pr_info("Error copying value to user.\n");
-            return -1;
+            retval = -1;
         }
         break;
     case WR_VALUE:
         if (copy_from_user(&answer, (int *) arg, sizeof(answer)) != 0) {
             pr_info("Error copying value from user.\n");
-            return -1;
+            retval = -1;
         } else {
             pr_info("Received answer = %d\n", answer);
         }
@@ -47,23 +55,24 @@ static long driver_unlocked_ioctl(struct file *file, unsigned int request, unsig
     case RD_STRUCT:
         if (copy_to_user((struct mystruct *) arg, &test, sizeof(test)) != 0) {
             pr_info("Error copying struct to user.\n");
-            return -1;
+            retval = -1;
         }
         break;
     case WR_STRUCT:
         if (copy_from_user(&test, (struct mystruct *) arg, sizeof(test)) != 0) {
             pr_info("Error copying struct from user.\n");
-            return -1;
+            retval = -1;
         } else {
             pr_info("Received num = %d, name = \"%s\"\n", test.num, test.name);
         }
         break;
     default:
         pr_info("Invalid request.\n");
-        return -1;
+        retval = -1;
     }
+    spin_unlock(&myspinlock);
 
-    return 0;
+    return retval;
 }
 
 static struct file_operations fops = {
@@ -80,6 +89,14 @@ static int __init module_start(void)
         pr_info("Error registering character device.\n");
         return -1;
     }
+    string = kzalloc(30, GFP_KERNEL); // use kmalloc if zeroing memory isn't necessary
+    if (!string) {
+        pr_info("Error allocating memory for string");
+        return -1;
+    }
+    strncpy(string, "Hello from testdriver.c!", 20);
+    pr_info("%s\n", string);
+    kfree(string);
     pr_info("Module loaded.\n");
     return 0;
 }
