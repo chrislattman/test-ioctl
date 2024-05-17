327396f72b8603bf4833c188c9c3f817b5c092d9
Chris Lattman <natgeo13@gmail.com>
2024-05-17 13:22:06 -0400

    Reorder imports

--- testdriver.c
+++ testdriver.c
@@ -1,11 +1,11 @@
-#include <linux/init.h>
-#include <linux/module.h>
 #include <linux/fs.h>
-#include <linux/uaccess.h>
+#include <linux/init.h>
 #include <linux/ioctl.h>
+#include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/string.h>
 #include <linux/spinlock.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
 
 #include "testdriver.h"
 
